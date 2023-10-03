5567cae6d83653da4b3181cf3a62d24847f7ed0a
Legaljay <adesinaayobami67@gmail.com>
2023-10-03 20:14:22 +0100

    worked on conditional rendering of the closing hours

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,29 @@
 export const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
     <footer className="footer">
-        {/* {new Date().toLocaleTimeString() `. We're currently open`}     */}
+      {isOpen && (
+        <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come visit us or order online.
+          </p>
+        </div>
+      )}
+        
     </footer>
   )
 }
+
+
+
+
+
+
+
+
+
+{/* {new Date().toLocaleTimeString() `. We're currently open`}     */}
