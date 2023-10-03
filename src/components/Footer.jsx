76c99c8af948162705f53758be4c4f1b3f749068
Legaljay <adesinaayobami67@gmail.com>
2023-10-03 20:16:18 +0100

    addded button to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,6 +11,7 @@ export const Footer = () => {
           <p>
             We're open until {closeHour}:00. Come visit us or order online.
           </p>
+          <button className="btn">Order</button>
         </div>
       )}
         
