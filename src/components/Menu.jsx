22ece86e5b07da5ef4cd911e6f7dd76f29fb68af
Legaljay <adesinaayobami67@gmail.com>
2023-10-03 20:34:44 +0100

    menu conditional rendering with ternary operator

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,19 +3,30 @@ import { pizzaData } from "./data"
 
 export const Menu = () => {
   const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
         <h2>Our menu</h2>
-        {numPizzas > 0 &&( 
+        {
+          numPizzas > 0 ? ( 
+        
+            <ul className="pizzas">
+                {pizzaData.map((pizza) =>  (
+                <Pizza key={pizza.name} pizzaObj={pizza}/>))}
+            </ul>
+          
+          ) : <p>We're still working on our menu. Please come back later :)</p>
+        }
+        {/* {numPizzas > 0 &&( 
         
           <ul className="pizzas">
               {pizzaData.map((pizza) =>  (
               <Pizza key={pizza.name} pizzaObj={pizza}/>))}
           </ul>
         
-        )}
+        )} */}
     </main>
   )
 }
