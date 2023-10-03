a649511bb5640e53edd94131f61721b5542453d2
Legaljay <adesinaayobami67@gmail.com>
2023-10-03 20:19:52 +0100

    added conditional rendering for the menu, so it only renders when there is pizza

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,13 +2,19 @@ import { Pizza } from "./Pizza"
 import { pizzaData } from "./data"
 
 export const Menu = () => {
+  const pizzas = pizzaData;
+
   return (
     <main className="menu">
         <h2>Our menu</h2>
-        <ul className="pizzas">
-            {pizzaData.map((pizza) =>  (
-            <Pizza key={pizza.name} pizzaObj={pizza}/>))}
-        </ul>
+        {pizzas &&( 
+        
+          <ul className="pizzas">
+              {pizzaData.map((pizza) =>  (
+              <Pizza key={pizza.name} pizzaObj={pizza}/>))}
+          </ul>
+        
+        )}
     </main>
   )
 }
